390451099cc0f684d0c704fa8048c34cc9fe8dca
Chandrakant <cchandrakant@tavisca.com>
2020-09-25 22:46:10 +0530

    Updates

---
+++ precache-manifest.db0e499e37176988af3185cbb1aea208.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22904f5482276d7b6fd3b9f056d2121e",
+    "url": "/billing-app/index.html"
+  },
+  {
+    "revision": "00da7bcca618e5f39436",
+    "url": "/billing-app/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "0048f3dd0a5e4bc639d4",
+    "url": "/billing-app/static/js/2.e76ee1ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/billing-app/static/js/2.e76ee1ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00da7bcca618e5f39436",
+    "url": "/billing-app/static/js/main.081e025a.chunk.js"
+  },
+  {
+    "revision": "98f678033ecda01821cf",
+    "url": "/billing-app/static/js/runtime-main.b0fcd54e.js"
+  }
+]);
